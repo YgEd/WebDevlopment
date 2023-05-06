898a78d098fb28ece5ce171747996bbd492d0dbc
YgEd <95714217+YgEd@users.noreply.github.com>
2023-05-06 11:01:26 -0400

    added working search feature

--- BackEnd/routes/search.js
+++ BackEnd/routes/search.js
@@ -26,7 +26,7 @@ router.post("/", async (req, res) => {
     let data = req.body
     console.log("post search route hit")
     console.log(data)
-    if (data.searchType != "user" && data.searchType != "group"){
+    if (help.strPrep(data.searchType).length == 0 || data.searchType != "user" && data.searchType != "group"){
         console.log("invalid search type")
         return res.redirect("/search")
     }
@@ -37,22 +37,255 @@ router.post("/", async (req, res) => {
     }
 
     if (data.searchType == "user"){
-        let userList = await userFuns.getAllUsers(100)
+        try{
+        var userList = await userFuns.getAllUsers(100)
+        } catch (error) {
+            console.log(error)
+            res.send({response: "db error"})
+        }
         //get usernames
-        userList = userList.map(value => value.username);
-        let target_user = closest(data.searchInput, userList)
-        console.log("target user found = " + target_user)
-        return res.send({response: target_user})
+        let usernames = userList.map(value => value.username);
+
+        //get closest username
+        let target_user = closest(data.searchInput, usernames)
+
+        //get user object
+        let userobj = userList.find(value => value.username == target_user)
+
+        //if user is authenticated
+        if (req.session.user && req.session.user.userName != target_user){
+            //check if user is already following
+            let curr_user = req.session.user.user_id
+            let following = false;
+            console.log("search post/ " + userobj.followers.includes(curr_user))
+            
+            //check if user is already following
+            for (let i = 0; i < userobj.followers.length; i++){
+                if (userobj.followers[i].toString() == curr_user.toString()){
+                    following = true;
+                }
+            }
+
+            return res.send({response: target_user, type: "user", userobj: userobj, following: following, auth:true})
+        }
+
+        return res.send({response: target_user, type: "user", userobj: userobj, auth: false})
     
     }
 
     if (data.searchType == "group"){
-        let groupList = await groupFuns.getAllGroups(100)
+        try {
+            var groupList = await groupFuns.getAllGroups(100)
+        } catch (error) {
+            console.log(error)
+            res.send({response: "db error"})
+        }
+        
         //get group names
-        groupList = groupList.map(value => value.groupName);
-        let target_group = closest(data.searchInput, groupList)
-        return res.send({response: target_group})
+        let groupname = groupList.map(value => value.groupName);
+
+        //get closest group name
+        let target_group = closest(data.searchInput, groupname);
+
+        //get group object
+        let groupobj = groupList.find(value => value.groupName == target_group)
+
+        //if user is authenticated
+        if (req.session.user){
+            //check if user is already in group
+            let curr_user = req.session.user.user_id
+            let inGroup = false;
+            
+            //check if user is already in group
+            for (let i = 0; i < groupobj.members.length; i++){
+                if (groupobj.members[i].toString() == curr_user.toString()){
+                    inGroup = true;
+                }
+            }
+
+
+            return res.send({response: target_group, type: "group", groupobj: groupobj, inGroup: inGroup, auth:true})
+        }
+
+        return res.send({response: target_group, type: "group", groupobj: groupobj, auth:false})
     }
+})
+
+router.post("/follow", async (req, res) => {
+    if (!req.session.user){
+        console.log("user not authenticated")
+        return res.redirect("/login")
+    }
+
+    let user_id = req.session.user.user_id
+    let data = req.body
+
+    if (help.strPrep(data.username).length == 0){
+        console.log("invalid username")
+        return res.send({response: "failed"})
+    }
+
+    try {
+
+        let userList = await userFuns.getAllUsers(100)
+        //get target user by username
+        let target_user = userList.find(value => value.username == data.username)
+        console.log("target_user: " + target_user._id)
+
+        //check if user is already following
+        for (let i = 0; i < target_user.followers.length; i++){
+            if (target_user.followers[i].toString() == user_id.toString()){
+                console.log("already following")
+                return res.send({response: "failed"})
+            }
+        }
+
+        //add user to target user's followers
+        await userFuns.addfollower(target_user._id, user_id)
+
+        return res.send({response: "success"})
+
+    } catch (error) {
+        console.log(error)
+        return res.send({response: "failed"})
+    }
+})
+
+router.post("/unfollow", async (req, res) => {
+    console.log("unfollow route hit")
+    if (!req.session.user){
+        console.log("user not authenticated")
+        return res.redirect("/login")
+    }
+
+    let user_id = req.session.user.user_id
+    let data = req.body
+
+    if (help.strPrep(data.username).length == 0){
+        console.log("invalid username")
+        return res.send({response: "failed"})
+    }
+
+    try {
+            
+            let userList = await userFuns.getAllUsers(100)
+            //get target user by username
+            let target_user = userList.find(value => value.username == data.username)
+    
+            //check if user is not following
+            for (let i = 0; i < target_user.followers.length; i++){
+                if (target_user.followers[i].toString() != user_id.toString() && i == target_user.followers.length - 1){
+                    console.log("not following")
+                    return res.send({response: "failed"})
+                }
+            }
+    
+            //remove user from target user's followers
+            await userFuns.removefollower(target_user._id, user_id)
+    
+            return res.send({response: "success"})
+    
+        } catch (error) {
+            console.log(error)
+            return res.send({response: "failed"})
+        }
+
+})
+
+
+router.post("/join", async (req, res) => {
+    if (!req.session.user){
+        console.log("user not authenticated")
+        return res.redirect("/login")
+    }
+
+    let user_id = req.session.user.user_id
+    let data = req.body
+
+    if (help.strPrep(data.groupname).length == 0){
+        console.log("invalid groupname")
+        return res.send({response: "failed"})
+    }
+
+    try {
+            
+            let groupList = await groupFuns.getAllGroups(100)
+            //get target group by groupname
+            let target_group = groupList.find(value => value.groupName == data.groupname)
+    
+            //check if user is already in group
+            for (let i = 0; i < target_group.groupMembers.length; i++){
+                if (target_group.groupMembers[i].toString() == user_id.toString()){
+                    console.log("already in group")
+                    return res.send({response: "failed"})
+                }
+            }
+    
+            //add user to target group's members
+            await groupFuns.addMember(target_group._id, user_id)
+    
+            return res.send({response: "success"})
+    
+        } catch (error) {
+            console.log(error)
+            return res.send({response: "failed"})
+        }
+
+})
+
+router.post("/leave", async (req, res) => {
+    if (!req.session.user){
+        console.log("user not authenticated")
+        return res.redirect("/login")
+    }
+
+    let user_id = req.session.user.user_id
+    let data = req.body
+
+    if (help.strPrep(data.groupname).length == 0){
+        console.log("invalid groupname")
+        return res.send({response: "failed"})
+    }
+
+    try {
+            
+            let groupList = await groupFuns.getAllGroups(100)
+            //get target group by groupname
+            let target_group = groupList.find(value => value.groupName == data.groupname)
+    
+            //check if user is not in group
+            for (let i = 0; i < target_group.groupMembers.length; i++){
+                if (target_group.groupMembers[i].toString() != user_id.toString() && i == target_group.groupMembers.length - 1){
+                    console.log("not in group")
+                    return res.send({response: "failed"})
+                }
+            }
+
+            //check if user is the owner
+            if (target_group.groupOwner.toString() == user_id.toString()){
+                console.log("user is owner")
+                try {
+                    //delete group
+                    await groupFuns.deleteGroup(target_group._id, user_id)
+                    res.send({response: "success"})
+                } catch (error) {
+                    console.log(error)
+                    return res.send({response: "failed"})
+                }
+            
+            }
+    
+            //remove user from target group's members
+            await groupFuns.removeMember(target_group._id, user_id)
+    
+            return res.send({response: "success"})
+    
+        } catch (error) {
+            console.log(error)
+            return res.send({response: "failed"})
+        }
+
+
 })
 
 
--- public/js/search.js
+++ public/js/search.js
@@ -1,3 +1,5 @@
+
+
 $( document ).ready(function() {
     console.log( 'ready!' );
     $("#search_button").click(function(e){
@@ -6,9 +8,26 @@ $( document ).ready(function() {
         let searchType = $("#searchType").val()
         let searchInput = $("#searchInput").val()
 
-        let search_data = {
-            "searchType": searchType,
-            "searchInput": searchInput
+        //clear previous search results
+        if ($(".search-res").length > 0){
+            $(".search-res").remove()
+        }
+
+        if (typeof searchType != "string" || (searchType != "user" && searchType != "group")){
+            alert("Invalid search type")
+            $("#searchType").val("") 
+            return
+        }
+
+        if (typeof searchInput != "string" ||  searchInput.trim().length == 0 || searchInput.trim().length > 30){
+            alert("Invalid search length")
+            $("#searchInput").val("")
+            return
+        }
+
+            let search_data = {
+            "searchType": searchType.trim(),
+            "searchInput": searchInput.trim(),
         }
 
         $.ajax({
@@ -18,14 +37,147 @@ $( document ).ready(function() {
             success: function (response){
                 console.log(response)
                 if (response.response == null){
-                    $("#search_result").html("<p class='center'>No results found</p>")
+                    $("#search_result").html("<p class='search-res center search-res-title'>No results found</p>")
                 } else {
-                    $("#search_result").html("<p class='center'>Result: " + response.response + "</p>")
+                    $("#search_result").append("<p class='search-res center search-res-title'>Result</p>")
+                    $("#search_result").append("<p class='center search-res search-name'>" + response.response + "</p>")
+                    
+                    //if user is authenticated
+                    if (response.auth){
+                        //add follow button
+                        if (response.type == "user"){
+                            if (response.following == true){
+                                $("#search_result").append("<button class='search-res center unfollow-button' type='submit'>Unfollow</button>")
+                            } else {
+                                $("#search_result").append("<button class='search-res center follow-button' type='submit'>Follow</button>")
+                            }
+                        }
+                        
+                        //add join button
+                        if (response.type == "group" && response.inGroup == false){
+                            if (response.inGroup == true){
+                                $("#search_result").append("<button class='search-res center leave-button' type='submit'>Leave</button>")
+                            } else {
+                                $("#search_result").append("<button class='search-res center join-button' type='submit'>Join</button>")
+                            }
+                        }
+                    }
                 }
 
             }})
 
     })
+
+    $(document).on('click', '.join-button', function (e) {
+        e.preventDefault();
+        console.log("join button clicked");
+
+        let postdata = {
+            "groupname": $(".search-name").text()
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "/search/join",
+            data: postdata,
+            success: function (response){
+                if (response.response == "success"){
+                    alert("Successfully joined group")
+                    window.location.reload()
+                } else {
+                    alert("Failed to join group")
+                    window.location.reload()
+                }
+            }
+
+
+        })
+
+    })
+
+    $(document).on('click', '.leave-button', function (e) {
+        e.preventDefault();
+        console.log("leave button clicked");
+
+        let postdata = {
+            "groupname": $(".search-name").text()
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "/search/leave",
+            data: postdata,
+            success: function (response){
+                if (response.response == "success"){
+                    alert("Successfully left group")
+                    window.location.reload()
+                } else {
+                    alert("Failed to leave group")
+                    window.location.reload()
+                }
+            }
+
+
+        })
+
+    })
+
+    $(document).on('click', '.follow-button', function (e) {
+        e.preventDefault();
+        console.log("follow button clicked");
+
+        let postdata = {
+            "username": $(".search-name").text()
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "/search/follow",
+            data: postdata,
+            success: function (response){
+                if (response.response == "success"){
+                    alert("Successfully followed user")
+                    window.location.reload()
+                    
+                } else {
+                    alert("Failed to follow user")
+                    window.location.reload()
+                }
+            }
+
+
+        })
+        
+    })
+
+
+    $(document).on('click', '.unfollow-button', function (e) {
+        e.preventDefault();
+        console.log("unfollow button clicked");
+
+        let postdata = {
+            "username": $(".search-name").text()
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "/search/unfollow",
+            data: postdata,
+            success: function (response){
+                if (response.response == "success"){
+                    alert("Successfully unfollowed user")
+                    window.location.reload()
+                } else {
+                    alert("Failed to unfollow user")
+                    window.location.reload()
+                }
+            }
+
+
+        })
+
+    })
+
 
 
 
